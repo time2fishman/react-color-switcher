dde2db99a992ff09254c3164c5d5c9da48a68ca3
time2fishman <adam_h_martinez@hotmail.com>
2022-12-09 14:35:50 -0700

    set up component structure

---
+++ src/components/ColorList.jsx
@@ -0,0 +1,8 @@
+const ColorList = () => {
+    return (
+        <div>
+            ColorList component
+        </div>
+    );
+}
+export default ColorList
---
+++ src/components/ColorListItem.jsx
@@ -0,0 +1,9 @@
+const ColorListItem = () => {
+    return (
+        <div>
+            ColorListItem Component            
+        </div>
+    );
+};
+
+export default ColorListItem;
